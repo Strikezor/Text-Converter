a9239a2595c1f93c4038504ca313a858ce94f8d0
Strikezor <akshaychaturvedi12345@gmail.com>
2024-07-12 10:51:48 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,47 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import Navbar from "./Components/Navbar";
+import TextForm from "./Components/TextForm";
+import React, { useState } from "react";
+import Alert from "./Components/Alert";
 
 function App() {
+  const [mode, setMode] = useState("light");
+  const [attension, setAttension] = useState(null);
+  const showAlert = (message, type) => {
+    setAttension({
+      msg: message,
+      type: type,
+    });
+    setTimeout(() => {
+      setAttension(null);
+    }, 2000);
+  };
+
+  const toggleMode = () => {
+    if (mode === "light") {
+      setMode("dark");
+      document.body.style.backgroundColor = "rgb(110 118 131)";
+      showAlert("Dark Mode", "success");
+    } else {
+      setMode("light");
+      document.body.style.backgroundColor = "white";
+      showAlert("Light Mode", "success");
+    }
+  };
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <Navbar title="TextConverter" mode={mode} toggleMode={toggleMode} />
+      <Alert attension={attension} />
+      <div className="container my-3">
+        {
+          <TextForm
+            showAlert={showAlert}
+            heading="Enter the text to analyze below"
+            mode={mode}
+          />
+        }
+      </div>
+    </>
   );
 }
-
 export default App;
